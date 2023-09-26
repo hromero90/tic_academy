d726320f76c89f5e7be915974d9a811f53f119f3
Hector <harelehg@gmail.com>
2023-09-26 10:05:35 -0600

    Aplicando estilos al index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,13 @@
 
 
     <section class="bg-cover" style="background-image: url({{asset('img/home/pexels-julia-m-cameron-4145190.jpg')}})">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            <div class="">
-                <h1 class="text-white font-fold text-3xl"> Bienvenidos a TIC Academy</h1>
-                <p class="text-white text-lg mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officia consequatur quibusdam qui voluptatem tempora sunt modi, soluta officiis quas laborum deserunt iusto illo, dolor placeat cum expedita dolorem nesciunt.</p>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
+            <div class="w-full md:w-3/4 lg:w1/2">
+                
+                <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Bienvenidos a <span class="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">TIC Academy</span></h1>
+                <p class="italic text-lg font-normal dark:text-white lg:text-xl">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+
+
 
                 <!-- component -->
                 <!-- This is an example component -->
@@ -15,13 +18,17 @@
                     <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                     type="search" name="search" placeholder="Search">
 
-                    <x-button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute right-0 top-0 mt-5 mr-4">
-                        Button
-                    </x-button>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute right-0 top-0 mt-2">
+                        Buscar
+                    </button>
 
                 </div>
             </div>
         </div>
+    </section>
+
+    <section class="mt-24">
+            <h1 class="text-gray-600 text-center text-3xl">Contenido</h1>
     </section>
     
 </x-app-layout>
