321ecc425706789846b086f5aa58a2ae3d77aa91
Hector <harelehg@gmail.com>
2023-09-26 00:06:11 -0600

    Estilos del Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,27 @@
 
 
 
-    <section style="background-image: url({{asset('img/home/pexels-julia-m-cameron-4144294.jpg')}})">
+    <section class="bg-cover" style="background-image: url({{asset('img/home/pexels-julia-m-cameron-4145190.jpg')}})">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div class="">
+                <h1 class="text-white font-fold text-3xl"> Bienvenidos a TIC Academy</h1>
+                <p class="text-white text-lg mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officia consequatur quibusdam qui voluptatem tempora sunt modi, soluta officiis quas laborum deserunt iusto illo, dolor placeat cum expedita dolorem nesciunt.</p>
 
-        hola mundo
+                <!-- component -->
+                <!-- This is an example component -->
+                
+                <div class="pt-2 relative mx-auto text-gray-600">
+                    <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                    type="search" name="search" placeholder="Search">
 
-        bienvenidos
+                    <x-button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute right-0 top-0 mt-5 mr-4">
+                        Button
+                    </x-button>
+
+                </div>
+            </div>
+        </div>
     </section>
     
 </x-app-layout>
+<!--Minuto 15: 28 del video--!
