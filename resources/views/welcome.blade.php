1104b0f0ab55839999833ef492be2da6c2c73746
Hector <harelehg@gmail.com>
2023-10-14 23:32:00 -0600

    Filtrar cursos por categoria y nivel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,44 +91,7 @@
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
             @foreach ($courses as $course)
-                <article class="bg-white shadow-lg rounded overflow-hidden">
-                    <img class="h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
-
-                    <div class="px-6 py-4">
-                        <h1 class="text-xl text-gray-700 mb-2 leading-6">{{Str::limit($course->title, 40)}}</h1>
-                        <p class= "text-gray-500 text-sm mb-2">Prof: {{$course->teacher->name}}</p>
-
-                        <div class="flex">
-                        
-                        <ul class="flex text-sm">
-                                <li class="mr-1">
-                                    <i class="fas fa-star text-{{$course->rating >= 1 ? 'green' : 'gray'}}-400"></i>
-                                </li>
-                                <li class="mr-1">
-                                    <i class="fas fa-star text-{{$course->rating >= 2 ? 'green' : 'gray'}}-400"></i>
-                                </li>
-                                <li class="mr-1">
-                                    <i class="fas fa-star text-{{$course->rating >= 3 ? 'green' : 'gray'}}-400"></i>
-                                </li>
-                                <li class="mr-1">
-                                    <i class="fas fa-star text-{{$course->rating >= 4 ? 'green' : 'gray'}}-400"></i>
-                                </li>
-                                <li class="mr-1">
-                                    <i class="fas fa-star text-{{$course->rating == 5 ? 'green' : 'gray'}}-400"></i>
-                                </li>
-                            </ul>
-                            <p class="text-sm text-gray-500 ml-auto">
-                                <i class="fas fa-users"></i>
-                                ({{$course->students_count}})
-                            </p>
-                        </div>
-                        <a href="{{route('courses.show', $course)}}" type="button" class=" block text-center w-full mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                            Mas Información...
-                        </a>
-
-                    </div>
-
-                </article>
+                <x-course-card :course="$course" />
             @endforeach
 
         </div>
