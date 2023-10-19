85809f2d95c01a0a5bf0437cdbdc4eb636ddc497
Hector <harelehg@gmail.com>
2023-10-18 23:58:48 -0600

    Recuperando informacion de las lecciones de los cursos

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,7 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+
     protected $guarded=['id'];
+
+    public function getCompletedAttribute(){
+        return $this->users->contains(auth()->user()->id);
+    }
 
     //Relacion de uno a uno
     public function description(){
