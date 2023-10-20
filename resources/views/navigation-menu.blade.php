3adfac242a247d0d96e28deefb1bfd3b3fff0248
Bayron Gutierrez <gbayron351@gmail.com>
2023-10-20 15:05:53 -0600

    Se genero rutas de acceso para los instructores

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -126,7 +126,11 @@
                                 </div>
 
                                 <x-dropdown-link href="{{ route('profile.show') }}">
-                                    {{ __('Profile') }}
+                                    Perfil
+                                </x-dropdown-link>
+
+                                <x-dropdown-link href="{{ route('instructor.courses.index') }}">
+                                    Instructor
                                 </x-dropdown-link>
 
                                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -203,7 +207,11 @@
                 <div class="mt-3 space-y-1">
                     <!-- Account Management -->
                     <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                        {{ __('Profile') }}
+                        Perfil
+                    </x-responsive-nav-link>
+
+                    <x-responsive-nav-link href="{{ route('instructor.courses.index') }}" :active="request()->routeIs('instructor.courses.index')">
+                        Instructor
                     </x-responsive-nav-link>
 
                     @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
