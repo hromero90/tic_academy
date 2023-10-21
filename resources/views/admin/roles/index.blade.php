3e2d53d452005049f5e79b6b7881c3a14d9eefb5
Bayron Gutierrez <gbayron351@gmail.com>
2023-10-21 01:30:02 -0600

    Continuacion de CRUD para roles

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -8,8 +8,50 @@
 
 @section('content')
     <div class="card">
+        <div class="card-header">
+
+            <a href="{{route('admin.roles.create')}}">Crear curso</a>
+
+        </div>
         <div class="card-body">
-            
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th colspan="2"></th>
+                    </tr>
+                    <tbody>
+                        @forelse ($roles as $role)
+                        <tr>
+                            <td>{{$role->id}}</td>
+                            <td>{{$role->name}}</td>
+
+                            <td>
+                                <a class="btn tbn-secondary" href="{{route('admin.roles.edit', $role)}}">Editar</a>
+                            </td>
+                            <td>
+                                <form action="{{route('admin.roles.destroy',$role)}}" method="POST">
+                                @method('delete')
+                                @csrf
+
+                                <button class="btn btn-danger" type="submit">Eliminar</button>
+                                
+                                </form>
+                            </td>
+                        </tr>
+                            
+                        @empty
+
+                        <tr>
+                            <td colspan="4">No hay ningún rol registrado</td>
+                        </tr>
+                            
+                        @endforelse
+                    </tbody>
+                </thead>
+            </table>
+
         </div>
     </div>
 @stop
